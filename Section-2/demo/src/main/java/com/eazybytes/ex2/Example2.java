72b7aff161c9cdaaf46acf23be9a1dda4c7006e2
KARTIK AHALAWAT <kartikahalawat01@gmail.com>
2026-02-14 14:00:17 +0530

    Naming Beans

---
+++ Section-2/demo/src/main/java/com/eazybytes/ex2/Example2.java
@@ -0,0 +1,21 @@
+package com.eazybytes.ex2;
+
+import com.eazybytes.ex2.beans.Vehicle;
+import com.eazybytes.ex2.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example2 {
+
+    static void main() {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        var veh = context.getBean("audiVehicle", Vehicle.class);
+        System.out.println("Vehicle name from Spring Context is: " + veh.getName());
+
+        var vehicle = (Vehicle) context.getBean("myFavouriteVehicle");
+        System.out.println("Vehicle name from Spring Context is: " + vehicle.getName());
+
+        var vhcle = context.getBean(Vehicle.class);
+        System.out.println("Vehicle name from Spring Context is: " + vhcle.getName());
+    }
+}
