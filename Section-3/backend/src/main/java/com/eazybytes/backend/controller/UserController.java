0dac231bdf1df7a72ef2cb3599e191019cd14a54
KARTIK AHALAWAT <kartikahalawat01@gmail.com>
2026-03-03 18:37:58 +0530

    RequestEntity & ResponseEntity

--- Section-3/backend/src/main/java/com/eazybytes/backend/controller/UserController.java
+++ Section-3/backend/src/main/java/com/eazybytes/backend/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.eazybytes.backend.controller;
 
+import com.eazybytes.backend.dto.UserDto;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -50,6 +55,23 @@ public class UserController {
     @GetMapping("/headers/map")
     public String readRequestHeadersWithMap(@RequestHeader Map<String, String> requestHeaders) {
         return "Recieved: " + requestHeaders.get("User-Agent") + " " + requestHeaders.get("User-Location");
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody UserDto userDto) {
+        return "Created user: " + userDto.toString();
+    }
+
+    @PostMapping("/request-entity")
+    public ResponseEntity<String> createUserRequestEntity(RequestEntity<UserDto> requestEntity) {
+        HttpHeaders headers = requestEntity.getHeaders();
+        UserDto userDto = requestEntity.getBody();
+        String queryParam = requestEntity.getUrl().getQuery();
+        String pathVariables = requestEntity.getUrl().getPath();
+        // return "Created user with the data: " + userDto.toString();
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header("Custom-Header", "ExampleValue")
+                .body("Created User with the data: " + userDto.toString());
     }
 
 }
